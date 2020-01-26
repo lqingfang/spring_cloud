a362ab1d5e97d2c9295504999fbd50c4406b00e2
lqingfang <lqingfang_279@163.com>
2020-01-27 03:03:38 +0800

    1、management.endpoints.web.exposure.include=*  用于开启actuator 2、异常处理(没跑通)

--- src/test/java/com/lqf/demo/DemoApplicationTests.java
+++ src/test/java/com/lqf/demo/DemoApplicationTests.java
@@ -1,10 +1,25 @@
 package com.lqf.demo;
 
+import com.lqf.demo.errorCodeDemo.CustomDuplicatedKeyException;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
+
+	@Autowired
+	public JdbcTemplate jdbcTemplate;
+
+	@Test(expected = CustomDuplicatedKeyException.class)
+	public void testThrowingCustomException() {
+		jdbcTemplate.execute("INSERT INTO FOO (ID, BAR) VALUES (1, 'a')");
+		jdbcTemplate.execute("INSERT INTO FOO (ID, BAR) VALUES (1, 'b')");
+	}
 
 	@Test
 	void contextLoads() {
